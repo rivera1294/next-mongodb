dcd413ca968beee55fd5c138b508bc133414bd18
Denis Poltoratsky <dpoltoratskiy@igatec.com>
2020-11-23 00:19:53 +0300

    MONGO_URI moved to envs; bundle analyzer added

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/zeit/next.js/tree/canary/packages/create-next-app).
 
-Update the `next.config.js` file with your mongodb connection string.
+## Getting Started
 
-```js
+### Development
 
-module.exports = {
-    env: {
-        MONGO_URI: "Your connection string"
-    }
-}
+`.env.dev`
 
+```bash
+MONGO_URI=<YOUR_MONGODB_CONNECTION_STRING>
 ```
 
-## Getting Started
+### Production
+
+`.env.prod`
+
+```bash
+MONGO_URI=<YOUR_MONGODB_CONNECTION_STRING>
+```
 
 First, run the development server:
 
@@ -22,7 +26,7 @@ npm run dev
 yarn dev
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+Open [http://localhost:9000](http://localhost:9000) with your browser to see the result.
 
 You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
 
--- next.config.js
+++ next.config.js
@@ -1,5 +1,33 @@
-module.exports = {
-    env: {
-        MONGO_URI: ""
-    }
+const webpack = require('webpack')
+const path = require('path')
+
+const fs = require('fs')
+const dotenv = require('dotenv')
+const isProduction = process.env.NODE_ENV === 'production'
+const envFileName = isProduction ? '.env.prod' : '.env.dev'
+const env = dotenv.parse(fs.readFileSync(envFileName))
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
+
+const nextConfig = {
+  webpack(config) {
+    config.resolve.alias['~'] = `${path.resolve(__dirname)}/`
+    config.plugins.push(new webpack.EnvironmentPlugin(['NODE_ENV']))
+
+    return config
+  },
+  env,
+  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: 'static',
+      reportFilename: '../analyze/server.html', // Относительно .next/server/
+    },
+    browser: {
+      analyzerMode: 'static',
+      reportFilename: './analyze/client.html', // Относительно .next/
+    },
+  },
 }
+
+module.exports = withBundleAnalyzer(nextConfig)
