e67a2443b82e1fd83677faa59b8084a04db131fd
Den Pol <selection4test@gmail.com>
2020-12-13 23:55:38 +0300

    fix

--- context/NotifsContext/NotifsContext.tsx
+++ context/NotifsContext/NotifsContext.tsx
@@ -1,15 +1,15 @@
 import { createContext, useContext, useMemo } from 'react'
-import { ReactNotificationProps as IReactNotificationProps } from 'react-notifications-component'
+import { ReactNotificationOptions as IReactNotificationOptions } from 'react-notifications-component'
 import { addInfoNotif as _addInfoNotif } from './addInfoNotif'
 import { addSuccessNotif as _addSuccessNotif } from './addSuccessNotif'
 import { useWindowSize } from '~/hooks'
 import ReactNotification from 'react-notifications-component'
 
 export const NotifsContext = createContext({
-  addInfoNotif: (_note: Partial<IReactNotificationProps>): void => {
+  addInfoNotif: (_note: Partial<IReactNotificationOptions>): void => {
     throw new Error('addInfoNotif method should be implemented')
   },
-  addSuccessNotif: (_note: Partial<IReactNotificationProps>): void => {
+  addSuccessNotif: (_note: Partial<IReactNotificationOptions>): void => {
     throw new Error('addSuccessNotif method should be implemented')
   },
 })
@@ -18,10 +18,10 @@ export const NotifsContextProvider = ({ children }: any) => {
   const { width } = useWindowSize()
   // @ts-ignore
   const isMobile = useMemo(() => (!!width ? width <= 767 : false), [width])
-  const addInfoNotif = (note: Partial<IReactNotificationProps>) => {
+  const addInfoNotif = (note: Partial<IReactNotificationOptions>) => {
     _addInfoNotif({ ...note })
   }
-  const addSuccessNotif = (note: Partial<IReactNotificationProps>) => {
+  const addSuccessNotif = (note: Partial<IReactNotificationOptions>) => {
     _addSuccessNotif(note)
   }
 
